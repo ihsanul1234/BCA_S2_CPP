1788243389a42143b67ad77884ea08959dd92c24
ihsanul1234 <ihsanulhaq2524@gmail.com>
2026-05-31 23:24:09 +0530

    added 2 basic progs helloworld and add two numbers

---
+++ cpp_prg2.cpp
@@ -0,0 +1,12 @@
+/*Author: Ihsanul Haq*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cout << "Enter two numbers" << endl;
+	cin >> a >> b;
+	cout << "sum = " << a + b << endl ;
+	return 0;
+}
+
